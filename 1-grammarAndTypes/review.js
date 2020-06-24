4376d5063718fb98f01f1e4f5bea937fa170a90e
Aurelius Eggerly <aureliuseggerly@Aureliuss-Air.hsd1.in.comcast.net>
2020-06-24 11:36:37 -0400

    inital commit

---
+++ 1-grammarAndTypes/review.js
@@ -0,0 +1,63 @@
+// Types
+
+ // Array
+ let myArray[]
+ let ticTacTo[
+     [0,0,0],
+     [0,0,0],
+     [0,0,0],
+ ];
+
+// Objects
+let myObject ={
+    key1: 'some value', key2: 2}
+
+//Booleans
+ true
+ false
+
+ 
+ // String
+ "double quotes"
+ 'single quotes'
+
+ // Number
+ 5
+ 3.14
+
+ // BigNum
+ 999999999999999n
+
+ // NULL
+
+
+
+
+ //what is a literal ?
+// A value that we have hard-coded into our program;
+
+//Variable types
+var x = 0;
+
+let y - 13; 
+
+y = 33;
+
+tank = 'hulk';
+
+const salesTax = 0.07;
+
+
+
+
+ // Assigment operators
+
+ // Assigment Operators
+ y = 56;
+
+ // Math operators
+ y += 4;
+ y -= 4;
+ y *= 3;
+ y /= 4;
+ y %= 2; // hint: this says a number is even if you get 0;
---
+++ 2-expressionsAndOperators/01-assignmentOperators.js
@@ -0,0 +1,24 @@
+// Assigment Operator
+let r = 5;
+// Addition assigment operator
+r = r +1;
+r += 1;
+r++;
+
+// Subtraction assigment operator
+r -= 7;
+r --;
+
+// Multiplicaction  assiment operator
+r *= 10;
+
+// Division assigment operator
+r /= 3;
+
+
+// Exponent assigment operator
+r **= 3;
+
+// Modulus assigment operator
+r %= 5;
+ // finds the remainder 
